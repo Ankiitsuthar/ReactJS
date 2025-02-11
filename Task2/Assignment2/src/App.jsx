45500d3bef80ac2413b58d886756729f6ac024a7
Ankit Kumar <Ankiitsuthar@gmail.com>
2025-02-11 23:05:38 +0530

    Added task2

---
+++ Task2/Assignment2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greetings from "./Compnents/Greet"
+import './App.css'
+const App = () => {
+  return (
+    <>
+      <Greetings/>
+    </>
+  )
+}
+
+export default App;
---
+++ Task2/Assignment2/src/Compnents/Greet.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Greet() {
+    return <h1>Hello, Welcome to Ankit's ReactJS!</h1>;
+}
+export default Greet;
